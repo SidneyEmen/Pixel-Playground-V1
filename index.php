99dfade32ad041adb27fafbeed8594bc59b849f5
SidneyEmen <6058166@mborijnland.nl>
2026-05-31 15:09:26 +0200

    Home Page H1

--- index.php
+++ index.php
@@ -7,3 +7,17 @@
 </body>
 
 <?php include 'includes/footer.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/PixelPlayground.css">
+</head>
+<body>
+    <h1>Welkom op Game Verse!</h1>
+    
+</body>
+</html>
