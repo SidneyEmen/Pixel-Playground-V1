151e8a5c4557047285c0bb89c2c76e88ff235396
LILFARICH16 <6054628@mborijnland.nl>
2026-06-04 17:27:32 +0200

    Beginning: HomePage

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php include 'includes/header.php'; ?>
 
 <main>
+    <h1 class="welcome_title">Welcome!</h1>
 
-    <h1>HOME</h1>
-    
+    <section class="sections_info">
+        <section class="website_info">
+            <h2>The Website</h2>
+            <p>[Info]</p>
+        </section>
+
+        <section class="highscore_info">
+            <h2>Latest Highscores</h2>
+            <p><strong>[Game_Name]</strong><br>[Username]: [Points]</p>
+        </section>
+    </section>
 </main>
+
 
 <?php include 'includes/footer.php'; ?>
 
